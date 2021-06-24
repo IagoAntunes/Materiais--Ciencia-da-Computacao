0eeade5c627ef634e96b77b3c883ed3a2a3d9a89
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-24 14:25:22 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 3ºPeriodo/Exercicios/Poo_4.cpp
@@ -0,0 +1,11 @@
+/*
+		setArea(){
+			this->area = getPi() * (getRaio() * getRaio());
+		}
+		setDistancia(){
+			this->distancia = getRaio() + getRaio2();
+		}
+		setCircunferencia(){
+			this->circunferencia = 2 * pi * getRaio();
+		}
+*/
