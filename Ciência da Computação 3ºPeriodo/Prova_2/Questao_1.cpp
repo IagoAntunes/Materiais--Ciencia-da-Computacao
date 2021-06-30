6c933bbf3d5859bdc83da86b7cd02e0f391929a4
Iago Antunes <iagoantunes.f@gmail.com>
2021-06-29 21:46:03 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 3ºPeriodo/Prova_2/Questao_1.cpp
+++ Ciência da Computação 3ºPeriodo/Prova_2/Questao_1.cpp
@@ -4,3 +4,71 @@ Implemente uma pequena aplicação para cadastro de motoristas, com funções de
  Para essa aplicação você pode definir quais atributos o registro terá,
   qual tipo de busca e qual será o campo chave definido.
   */
+ 
+#include<iostream>
+using namespace std;
+
+
+class motorista{
+	private:
+		string nome;
+		int carteira;
+		int Pcarteira;
+
+	public:
+		//Armazena o Nome
+		int setNome(string nome){
+			this->nome = nome;
+		}
+		//Armazena a Carteira
+		int setCarteira(int carteira){
+			this->carteira = carteira;
+		}
+		//Retorna a carteira
+		int getCarteira(){
+			return this->carteira;
+		}	
+};
+
+
+
+int main(){
+	motorista m[100];
+	string nome;
+	int carteira;
+	int Pcarteira;
+	int i =0;
+	int opc = 0;
+
+	
+	while(opc != -1){
+		
+		//Escolha do usuario
+		cout<<"[1]-Adicionar   [2]Procurar " <<endl;
+		cin>>opc;
+		
+		//Opcao para adicionar motorista e carteira
+		if(opc == 1){
+			cout<<"Nome Motorista: " <<endl;
+		    cin>>nome;
+		    cout<<"Carteira Motorista: "<<endl;
+		    cin >> carteira;
+		    m[i].setCarteira(carteira);
+		    m[i].setNome(nome);
+		    i++;
+			
+		}
+		//Opcao para procurar carteira
+	    if(opc == 2){
+	    	
+	    	cout<<"Procurar Carteira: "<<endl;
+    		cin >> Pcarteira;
+    		for(int j=0;j<i;j++){
+    			if(m[j].getCarteira()==Pcarteira){
+    				cout<<"Carteira Encontrada"<<endl;
+				}
+			}		
+		}
+	}
+}
+	
--- Ciência da Computação 3ºPeriodo/Prova_2/Questao_2.cpp
+++ Ciência da Computação 3ºPeriodo/Prova_2/Questao_2.cpp
@@ -1,6 +1,61 @@
-/*
-Crie uma classe para descrever RESTAURANTE. Os membros devem armazenar o nome, o endereço, o preço médio e o tipo de comida.
-a) Crie um construtor que inicialize os dados com zero e outro construtor que inicialize os dados com um valor fixo que deverá ser passado como parâmetro.
-b) Crie um método (função membro) para solicitar os dados para o usuário.
-c) Crie um método (função membro) para imprimir os dados de um restaurante.
-*/
+#include<iostream>
+using namespace std;
+
+
+class restaurante{
+	private:
+		//Variaveis
+		string nome;
+		string endereco;
+		float pMedio;
+		string comida;
+
+	public:
+		//Inicializa
+		void inicializa(){
+            this->nome = "";
+            this->endereco = "";
+            this->pMedio = 0.0;
+            this->comida = "";
+        };
+        //Armazena todas as variaveis
+        void completa(string nome, string endereco,float pMedio,string comida){
+            this->nome = nome;
+            this->endereco = endereco;
+            this->pMedio = pMedio;
+            this->comida = comida;
+        }
+        //Solicita os Dados
+        void entradas(){
+        	cout << "Nome Restaurante:  " <<endl;
+			getline(cin,nome);
+			cout << "Endereco:  " <<endl;
+			getline(cin,endereco);
+			cout << "Preco Medio:  " << endl;
+			cin >> pMedio;
+			cout << "Tipo Comida: " << endl;
+			cin >> comida;
+        	
+      		completa(nome,endereco,pMedio,comida);//Armazena os dados
+		}
+		//Imprimir os Dados
+		void imprimir(){
+			system("cls || clear");
+			cout << "Nome:  " <<nome<<endl;
+			cout << "Endereco:  " <<endereco<<endl;
+			cout << "Preco Medio:  " <<pMedio<< endl;
+			cout << "Tipo Comida:  " <<comida<< endl;
+		}	
+};
+
+
+
+int main(){
+	restaurante r;
+	
+	r.inicializa();//Inicializa
+	r.entradas();//Entrada dos dados
+	r.imprimir();//Imprimir
+
+}
+	
