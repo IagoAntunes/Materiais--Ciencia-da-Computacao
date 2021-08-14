be77a751f91aa1bddd2e496858b6362cf349f2c2
Iago Antunes <iagoantunes.f@gmail.com>
2021-08-13 21:53:30 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/Helloworld.java
@@ -0,0 +1,12 @@
+
+
+class Simple {  
+   public static void main(String args[]) {  
+      System.out.println("Hello Java,");  
+      String nome = "ola MUNDO";
+
+
+      int tamanho = nome.length();
+      System.out.print("Tamanho = " + tamanho);  
+   }  
+}  
