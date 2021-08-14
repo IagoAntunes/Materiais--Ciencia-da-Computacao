5840419581c78a471ed1a5fa7d738cc3573dde8e
Iago Antunes <iagoantunes.f@gmail.com>
2021-08-13 22:19:47 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/Ex2.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+
+class caneta{
+
+    String cor;
+    int tamanho;
+    int quantidade;
+    int escreveu;
+
+    public void inicializando(String cor,int tamanho,int quantidade,int escreveu){
+        cor = " ";
+        tamanho = 0;
+        quantidade = 0;
+        escreveu = 0;
+    }
+
+    public void printar(String cor,int tamanho,int quantidade){
+        System.out.println("Cor = "+cor+"Tamanho= "+tamanho+"Quantidade="+quantidade+"Escreveu: +");
+        System.out.println("Cor = {0}",cor);
+    }
+
+    public void escrever(int escrever){
+        escrever ++;
+        System.out.println("Escrevendo...");
+    }
+    public void changeColor(String color){
+        this.cor = color;
+    }
+}
+
+class Ex2 {  
+    public static void main(String args[]) {  
+        caneta caneta1 = new caneta();
+        Scanner input = new Scanner(System.in);
+
+        caneta1.inicializando(caneta1.cor,caneta1.tamanho,caneta1.quantidade,caneta1.escreveu);
+        caneta1.cor = "Azul";
+        caneta1.tamanho = 2;
+        caneta1.quantidade = 4;
+
+        caneta1.changeColor("teste");
+        caneta1.escrever(caneta1.escreveu);
+        caneta1.printar(caneta1.cor,caneta1.tamanho,caneta1.quantidade);
+
+    }  
+ } 
