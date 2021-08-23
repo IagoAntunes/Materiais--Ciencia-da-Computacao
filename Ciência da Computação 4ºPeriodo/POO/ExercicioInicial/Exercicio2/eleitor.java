94a4cb72dcfa4f74ce2e7769172fd569d34f5399
Iago Antunes <iagoantunes.f@gmail.com>
2021-08-23 12:02:46 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/ExercicioInicial/Exercicio2/eleitor.java
@@ -0,0 +1,39 @@
+package ExercicioInicial.Exercicio2;
+
+public class eleitor {
+    
+    public Integer idade;
+    public String sexo;
+    public String nome;
+    public Integer numero;
+
+
+    public void setIdade(Integer idade){
+        this.idade = idade;
+    }
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setNumero(Integer numero){
+        this.numero = numero;
+    }
+    public int getIdade(){
+        return this.idade;
+    }
+    public String getSexo(){
+        return this.sexo;
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+
+
+
+
+}
--- Ciência da Computação 4ºPeriodo/POO/ExercicioInicial/Exercicio2/main.java
+++ Ciência da Computação 4ºPeriodo/POO/ExercicioInicial/Exercicio2/main.java
@@ -1,5 +1,26 @@
 package ExercicioInicial.Exercicio2;
-
+import java.util.Scanner;
 public class main {
     
+    public static void main(String args[]){
+
+        eleitor eleitor  = new eleitor();
+        Scanner input = new Scanner(System.in); 
+
+        eleitor.setNome(input.nextLine());
+        eleitor.setIdade(input.nextInt());
+        eleitor.setSexo(input.nextLine());
+        eleitor.setNumero(input.nextInt());
+
+        if(eleitor.getIdade() <16){
+            System.out.println("Você não pode votar");
+        }else if(eleitor.getIdade() < 18){
+            System.out.println("Desobrigada a votar");
+        }else if(eleitor.getIdade() >= 18){
+            System.out.println("Obrigado a votar!!");
+        }
+
+
+
+    }
 }
