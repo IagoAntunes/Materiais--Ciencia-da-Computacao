14d285f87c1e0642979442605a7d22212e03731d
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-03 19:57:29 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/Exercicio5/main.java
@@ -0,0 +1,16 @@
+package Exercicio5;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+//.charAt(0)
+class Exercicio5 {
+    public static void main(String args[]) { 
+        Scanner input = new Scanner(System.in);
+
+
+        //descomto = teste ?v : f
+
+
+    }
+}
