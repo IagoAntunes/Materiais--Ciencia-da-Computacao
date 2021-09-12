6a2aa587cb3e7c0713d75d88e16d3d0fd2f67ed8
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-11 23:47:15 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/Exercicio8/main.java
@@ -0,0 +1,25 @@
+package Exercicio8;
+import java.util.Scanner;
+
+import ExercicioInicial.Exercicio1.trab;
+
+
+public class main {
+    public static void main(String args[]) { 
+        Scanner input = new Scanner(System.in);
+
+        trabalhador[] trabalhador = new trabalhador[3];
+
+
+        for(int i =0;i<3;i++){
+            System.out.println("T"+i+" Nome: ");
+            trabalhador[i].setNome(input.nextLine());
+            System.out.println("T"+i+" Salário: ");
+            trabalhador[i].setSalario(input.nextDouble());
+            System.out.println("T"+i+" Matricula: ");
+            trabalhador[i].setMatricula(input.nextInt());
+        }
+
+
+    }
+}
