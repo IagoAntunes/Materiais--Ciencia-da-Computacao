351a110b18ce394ea9755cf667e897d48e7338bc
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-17 19:44:53 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 4ºPeriodo/POO/ExercicioAvaliativo/Exercicio4/main.java
+++ Ciência da Computação 4ºPeriodo/POO/ExercicioAvaliativo/Exercicio4/main.java
@@ -1,10 +1,27 @@
 package ExercicioAvaliativo.Exercicio4;
+import java.sql.Time;
 import java.util.Scanner;
 public class main {
     public static void main(String args[]) { 
+        Scanner input = new Scanner(System.in);
 
-        time time = new time("Galo", "A");
+        System.out.println("Digite o nome do time: ");
+        String time1 = input.nextLine();
+        System.out.println("Digite a serie:");
+        String serie1 = input.nextLine();
+        System.out.println("Numero de jogadores:");
+        Integer nJogadores1 = input.nextInt();
+        time time = new time(time1, serie1,nJogadores1);
+        time.mensagem();
 
+        System.out.println("Digite o nome do time: ");
+        String time2 = input.nextLine();
+        System.out.println("Digite a serie:");
+        String serie2 = input.nextLine();
+        System.out.println("Numero de jogadores:");
+        Integer nJogadores2 = input.nextInt();
+        time timedois = new time(time2, serie2, nJogadores2);
+        timedois.mensagem();
 
     }
 }
--- Ciência da Computação 4ºPeriodo/POO/ExercicioAvaliativo/Exercicio4/time.java
+++ Ciência da Computação 4ºPeriodo/POO/ExercicioAvaliativo/Exercicio4/time.java
@@ -6,12 +6,40 @@ public class time{
     private String serie;
     private Integer nJogadores;
 
-    public time(String nome,String serie){
+    public time(String nome,String serie,Integer nJogadores){
         this.nome = nome;
-        this.serie = serie;
+        setSerie(serie);
+        setNJogadores(nJogadores);
     }
-    
+    public void setNJogadores(Integer nJogadores){
+        if(nJogadores < 22){
+            this.nJogadores = nJogadores;
+        }else{
+            System.out.println("Numero jogadores maximo!!");
+        }
+    }
+    public void setSerie(String serie){
+        if(serie.equals("A") || serie.equals("B") || serie.equals("C") || serie.equals("D") || serie.equals("E")){
+            this.serie = serie;
+        }else{
+            System.out.println("Serie não encontrada!!");
+        }
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public String getSerie(){
+        return this.serie;
+    }
+    public Integer getnJogadores(){
+        return this.nJogadores;
+    }
+
+    public String mensagem(){
+        return ("\nNome:"+getNome()+"\nSerie:"+getSerie()+"\nNjogadores:"+getnJogadores());
+    }
 
 
 
+    ///aaaa
 }
