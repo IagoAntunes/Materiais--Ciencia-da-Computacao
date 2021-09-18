69c3aca0ee6b4bd977e56186826d8bd508b5c94e
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-18 03:52:20 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 4ºPeriodo/POO/Prova1/Questao3/main.java
+++ Ciência da Computação 4ºPeriodo/POO/Prova1/Questao3/main.java
@@ -1,3 +1,9 @@
+/*
+Questão 3 – 2 pontos -Reescreva as classes Conta e TestaConta aplicando o encapsulamento dos 
+atributos da classe Conta, ou seja, modificando o acesso dos mesmos para apenas a própria 
+classe e criando métodos de acesso.
+*/
+
 package Prova1.Questao3;
 
 public class main {
