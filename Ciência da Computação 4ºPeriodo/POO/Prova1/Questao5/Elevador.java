9691b339eb3d7307ca36c90e0ae64b2e46c4e163
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-18 03:41:57 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 4ºPeriodo/POO/Prova1/Questao5/Elevador.java
+++ Ciência da Computação 4ºPeriodo/POO/Prova1/Questao5/Elevador.java
@@ -1,13 +1,70 @@
 package Prova1.Questao5;
 
+import Exercicio4.pessoa;
+
 public class Elevador {
-    
+    //Variaveis
     public int andarAtual;
     public int totalAndares;
     public int capacidadeElevador;
     public int pessoasElevador;
+    //Metodos
+    public Elevador(int capacidadeElevador,int totalAndares){
+        this.capacidadeElevador = capacidadeElevador;
+        this.totalAndares = totalAndares;
 
-
-    
+        this.andarAtual = 0;
+        this.pessoasElevador = 0;
+    }
+    public void Entra(){
+        if(getPessoasElevador() < this.capacidadeElevador){
+            this.pessoasElevador ++;
+        }
+    }
+    public void Sai(){
+        if(getPessoasElevador() >= 1){
+            this.pessoasElevador --;
+        }
+    }
+    public void Sobe(){
+        if(getAndarAtual() < this.totalAndares){
+            this.andarAtual ++;
+        }
+    }
+    public void Desce(){
+        if(getAndarAtual() >= 1){
+            this.andarAtual --;
+        }
+    }
+    //Outros
+    public String Mensagem(){
+        return ("Andar Atual: "+getAndarAtual()+"\nTotal de Andares: "+getTotalAndares()+"\nCapacidadeElevador: "+getCapacidadeElevador()+"\nPessoas no Elevador: "+getPessoasElevador()+"\n\n\n\n\n");
+    }
+    //Get
+    public int getTotalAndares(){
+        return this.totalAndares;
+    }
+    public int getCapacidadeElevador(){
+        return this.capacidadeElevador;
+    }
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+    public int getPessoasElevador() {
+        return pessoasElevador;
+    }
+    //Set
+    public void setTotalAndares(int totalAndares){
+        this.totalAndares = totalAndares;
+    }
+    public void setCapacidadeElevador(int capacidadeElevador){
+        this.capacidadeElevador = capacidadeElevador;
+    }
+    public void setAndarAtual(int andarAtual){
+        this.andarAtual = andarAtual;
+    }
+    public void setPessoaElevador(int pessoasElevador){
+        this.pessoasElevador = pessoasElevador;
+    }
 
 }
--- Ciência da Computação 4ºPeriodo/POO/Prova1/Questao5/main.java
+++ Ciência da Computação 4ºPeriodo/POO/Prova1/Questao5/main.java
@@ -1,11 +1,11 @@
 /*
 Crie uma classe denominada Elevador para armazenar as informações de um 
 elevador dentro de um prédio.
- A classe deve armazenar o andar atual (térreo = 0), total de andares 
+
+(FEITO) A classe deve armazenar o andar atual (térreo = 0), total de andares 
 no prédio, excluindo o térreo, capacidade do elevador, e quantas pessoas estão presentes nele. 
 
-A 
-classe deve também disponibilizar os seguintes métodos: 
+A classe deve também disponibilizar os seguintes métodos: 
 Inicializa(Construtor): que deve receber como parâmetros a capacidade do elevador e o total de 
 andares no prédio (os elevadores sempre começam no térreo e vazio); 
 Entra: para acrescentar uma pessoa no elevador (só deve acrescentar se ainda houver espaço); 
@@ -21,12 +21,35 @@ import java.util.Scanner;
 public class main {
     public static void main(String args[]) { 
         Scanner input = new Scanner(System.in);
+        int opc;
+        System.out.println("Capacidade do Elevador:");
+        int capacidadeElevador = input.nextInt();
+        System.out.println("Total Andares: ");
+        int totalAndares = input.nextInt();
 
+        Elevador elevador = new Elevador(capacidadeElevador, totalAndares);
 
-
-
-
-
-
+        do{
+            System.out.println("[1]Entrar\n[2]Sair\n[3]Subir\n[4]Descer");
+            opc = input.nextInt();
+            switch(opc){
+                case 1:
+                    elevador.Entra();
+                    break;
+                case 2:
+                    elevador.Sai();
+                    break;
+                case 3:
+                    elevador.Sobe();
+                    break;
+                case 4:
+                    elevador.Desce();
+                    break;
+                default:
+                    System.out.println("Algo de Errado :P");
+            }
+            System.out.println(elevador.Mensagem());
+        }while(opc != 5);
     }
 }
+
