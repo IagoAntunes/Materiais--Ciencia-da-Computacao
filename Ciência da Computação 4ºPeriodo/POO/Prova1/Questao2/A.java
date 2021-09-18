e83d701ef8f0a99a1b930f034ee155165acdf553
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-18 03:51:57 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 4ºPeriodo/POO/Prova1/Questao2/A.java
+++ Ciência da Computação 4ºPeriodo/POO/Prova1/Questao2/A.java
@@ -1,3 +1,18 @@
+/*
+Questão 2 – 2 pontos - Considere a classe A abaixo. Implemente o método "m" conforme as 
+seguintes regras: o método
+retorna true se o valor proposto no parâmetro de m for maior que zero. O método retorna false se o
+valor proposto no parâmetro de m for menor ou igual à zero. Se o método retornar true então ele
+deve, também, alterar o valor do atributo a para 10 vezes o valor proposto no parâmetro do método
+m. 
+public class A {
+    int a;
+    public boolean m(int valor) {
+    // ?
+    }
+}   
+*/
+
 package Prova1.Questao2;
 
 public class A {
