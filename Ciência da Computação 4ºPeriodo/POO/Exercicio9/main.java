4aca9bcd971fdadb9cf774c860f0bc7393a878a2
Iago Antunes <iagoantunes.f@gmail.com>
2021-09-24 19:54:23 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/Exercicio9/main.java
@@ -0,0 +1,11 @@
+package Exercicio9;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[]) { 
+        Scanner input = new Scanner(System.in);
+
+
+
+    }
+}
