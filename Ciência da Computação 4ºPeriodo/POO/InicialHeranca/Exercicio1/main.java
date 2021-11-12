00c2b0a292c72ef16e3ba1a5566a8833bacab658
iagoa <iagoantunes.f@gmail.com>
2021-11-12 19:59:24 -0300

    Iago Antunes Ferreira

--- Ciência da Computação 4ºPeriodo/POO/InicialHeranca/Exercicio1/main.java
+++ Ciência da Computação 4ºPeriodo/POO/InicialHeranca/Exercicio1/main.java
@@ -21,52 +21,56 @@ Crie uma classe Animal que obedeça à seguinte descrição:
 (feito)Crie ainda os métodos get e set para o atributo caracteristica. 
 (feito)Crie um método dadosPeixe sem parâmetro e do tipo void, que, quando chamado, imprime
 (feito)na tela uma espécie de relatório informando os dados do peixe (incluindo os dados do Animal e mais a característica).
-
+//iago antunes ferreira
 3-
-(feito)Crie uma classe Mamifero que herde da classe Animal e obedeça à seguinte descrição:  possua um atributo alimento(String) 
+(feito)Crie uma classe Mamifero que herde da classe Animal e obedeça à seguinte descrição:  
+(feito)possua um atributo alimento(String) 
 (feito)Crie um método construtor que receba por parâmetro os valores iniciais de cada um dos atributos
 (feito)(incluindo os atributos da classe Animal) e atribua-os aos seus respectivos atributos. 
 (feito)Crie ainda os métodos get e set para o atributo alimento. 
 (feto)Crie um método dadosMamifero sem parâmetro e do tipo void, que, quando chamado, imprime na tela
-uma espécie de relatório informando os dados do mamifero (incluindo os dados do Animal e mais o alimento).
+(feito)uma espécie de relatório informando os dados do mamifero (incluindo os dados do Animal e mais o alimento).
 
 4-
-Crie uma classe TestarAnimais possua um método main para testar as classes criadas.
-Crie um objeto camelo do tipo Mamífero e atribua os seguintes valores para seus atributos:  Nome: Camelo  Comprimento: 150 cm  Patas: 4  Cor: Amarelo  Ambiente: Terra  Velocidade: 2.0 m/s
-Crie um objeto tubarao do tipo Peixe e atribua os seguintes valores para seus atributos  Nome: Tubarão  Comprimento: 300 cm  Patas: 0  Cor: Cinzento  Ambiente: Mar  Velocidade: 1.5 m/s  Caracteristica: Barbatanas e cauda
-Crie um objeto ursocanada do tipo Mamifero e atribua os seguintes valores para seus atributos:  Nome: Urso-do-canadá  Comprimento: 180 cm  Patas: 4  Cor: Vermelho  Ambiente: Terra  Velocidade: 0.5 m/s  Alimento: Mel
-Chame os método para imprimir os dados de cada um dos objetos criados.
+(feito)Crie uma classe TestarAnimais possua um método main para testar as classes criadas.
+(feito)Crie um objeto camelo do tipo Mamífero e atribua os seguintes valores para seus atributos:
+(feito) Nome: Camelo  Comprimento: 150 cm  Patas: 4  Cor: Amarelo  Ambiente: Terra  Velocidade: 2.0 m/s
+(feito)Crie um objeto tubarao do tipo Peixe e atribua os seguintes valores para seus atributos 
+(feito) Nome: Tubarão  Comprimento: 300 cm  Patas: 0  Cor: Cinzento  Ambiente: Mar  Velocidade: 1.5 m/s  Caracteristica: Barbatanas e cauda
+(feito)Crie um objeto ursocanada do tipo Mamifero e atribua os seguintes valores para seus atributos: 
+(feito) Nome: Urso-do-canadá  Comprimento: 180 cm  Patas: 4  Cor: Vermelho  Ambiente: Terra  Velocidade: 0.5 m/s  Alimento: Mel
+(feito)Chame os método para imprimir os dados de cada um dos objetos criados.
 
 5-
 Crie na classe animal o método emitirSom() que retorna uma string com o som do animal. 
 Este método deve ser sobreescrito em cada animal que herda desta classe.
 
 */
-public class main {
+public class main {//Iago Antunes Ferreira
   public static void main(String args[]) {
     Scanner input = new Scanner(System.in);
 
+    //Inicializando
+    Mamifero camelo = new Mamifero("Camelo",150,4,"Amarelo","Terra",2.0);
+    Peixe tubarao = new Peixe("Tubarao", 300, 0, "Cinzento", "Mar", 1.5,"Barbatanas e cauda");
+    Mamifero ursocanada = new Mamifero("Urso-do-canadá", 180, 4, "Vermelho", "Terra", 0.5,"Mel");
 
-    System.out.println("Nome:");
-    String nome = input.nextLine();
-    System.out.println("Comprimento:");
-    Float comprimento = input.nextFloat();
-    input.nextLine();
-    System.out.println("Numero de Patas:");
-    Integer nPatas = input.nextInt();
-    input.nextLine();
-    System.out.println("Cor:");
-    String cor = input.nextLine();
-    input.nextLine();
-    System.out.println("Ambiente:");
-    String ambiente = input.nextLine();
-    System.out.println("Velocidade Media:");
-    Float vMedia = input.nextFloat();
-    input.nextLine();
+    //Impressoes
+    System.out.println(camelo.dadosMamifero());
+    System.out.println(tubarao.dadosPeixe());
+    System.out.println(ursocanada.dadosMamifero());
 
-    animais animal = new animais(nome, comprimento, nPatas, cor, ambiente, vMedia);
+    //Sons animais
+    camelo.setSom("30");
+    tubarao.setSom("5");
+    ursocanada.setSom("60");
 
-    System.out.println(animal.toString());
+    //Emitindo sons 
+    System.out.println("\n\nCamelo emitiu "+camelo.emitirSom()+" Som...");
+    System.out.println("Tubarao emitiu "+tubarao.emitirSom()+" Som....");
+    System.out.println("Urso emitiu "+ursocanada.emitirSom()+" Som...");
+
+
 
   }
 }
