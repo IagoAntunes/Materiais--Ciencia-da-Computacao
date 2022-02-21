191b98ff4edcbbfaa2f8ea077ed9f5789870b89e
iagoa <iagoantunes.f@gmail.com>
2022-02-20 21:30:58 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 4ºPeriodo/POO/ProvaFinal0/main.java
@@ -0,0 +1,11 @@
+package ProvaFinal0;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("OLA");
+    }
+}
