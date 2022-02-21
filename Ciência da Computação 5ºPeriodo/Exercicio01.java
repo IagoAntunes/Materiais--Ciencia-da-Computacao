3fbf424b6a0125c443c6a5c2926d20049bf9bcc1
iagoa <iagoantunes.f@gmail.com>
2022-02-20 21:46:17 -0300

    teste

---
+++ Ciência da Computação 5ºPeriodo/Exercicio01.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        ArrayList
+        
+        int qtdLetras = input.nextInt();
+        String resp = input.nextLine();
+
+        
+
+
+
+
+        System.out.println("OLA EXERCICIO01");
+    }
+}
