c88a292a002c220a3cc2265cf53c7ebd4d2a1ff4
iagoa <iagoantunes.f@gmail.com>
2022-03-20 01:30:44 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 5ºPeriodo/Ciencia de Dados/ListaEtapa1/Exercicio02.py
@@ -0,0 +1,15 @@
+#– Faça um programa que leia um número n e em seguida leia n inteiros. Crie uma lista 
+#ordenada sem elementos repetidos a partir da lista original e mostre a lista original e a lista 
+#ordenada sem elementos repetidos na tel
+
+n = int(input("Digite: "))
+lista = []
+listaFinal = []
+
+for x in range(n):
+    num = int(input("Num: "))
+    lista.append(num)
+
+listaFinal = set(lista)
+print(lista)
+print(listaFinal)
