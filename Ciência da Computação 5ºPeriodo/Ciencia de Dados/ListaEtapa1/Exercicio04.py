ca488f03aaee6c30a32b8b102c003c73f7d931af
iagoa <iagoantunes.f@gmail.com>
2022-03-20 03:12:54 -0300

    Iago Antunes Ferreira

---
+++ Ciência da Computação 5ºPeriodo/Ciencia de Dados/ListaEtapa1/Exercicio04.py
@@ -0,0 +1,22 @@
+#Leia um texto de um arquivo e crie um dicionário que conte o número de vezes que cada 
+#letra apareceu no texto. Crie seu próprio arquivo de teste
+
+
+
+dic = {}
+junta = ""
+file = open('Ciencia de Dados\\ListaEtapa1\\arq', 'r')
+
+for palavra in file:
+    junta += palavra
+
+palavra2 = junta.replace("\n","").replace(" ","").upper()
+
+for letra in palavra2:
+    if(letra.isdigit):
+        if(letra in dic):
+            dic[letra] = dic.get(letra,0)+1
+        else:
+            dic[letra] = dic.get(letra,0)+1
+
+print(dic)
